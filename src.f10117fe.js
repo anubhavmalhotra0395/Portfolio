f4dc4f8f4a04747763fce634448ee60036091613
anubhavmalhotra0395 <malhotraanubhav0395@gmail.com>
2023-03-07 00:49:58 +0530

    Updates

--- src.f10117fe.js
+++ src.f10117fe.js
@@ -71367,7 +71367,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/fonts/garet/Garet-Heavy.ttf":[["Garet-Heavy.3e9505c1.ttf","assets/fonts/garet/Garet-Heavy.ttf"],"assets/fonts/garet/Garet-Heavy.ttf"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/gsap/gsap-core.js":[function(require,module,exports) {
+},{"./../assets/fonts/Azonix.otf":[["Azonix.7f606a19.otf","assets/fonts/Azonix.otf"],"assets/fonts/Azonix.otf"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/gsap/gsap-core.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -80296,7 +80296,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61786" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64228" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
