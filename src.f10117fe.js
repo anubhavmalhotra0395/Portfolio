15958d807129e4d2ea737f20032bda7abf106a37
anubhavmalhotra0395 <malhotraanubhav0395@gmail.com>
2023-03-07 14:16:58 +0530

    Updates

--- src.f10117fe.js
+++ src.f10117fe.js
@@ -80296,7 +80296,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53720" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59891" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
